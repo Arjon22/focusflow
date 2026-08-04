2f7b67331963793af9cdd451312c3a701c578660
Arjon22 <arjontalukder100@gmail.com>
2026-08-05 01:54:25 +0600

    Add page routing and mobile navigation

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,19 @@
+function Settings() {
+
+  return (
+    <div>
+
+      <h1>
+        ⚙️ Settings
+      </h1>
+
+      <p>
+        App settings will appear here.
+      </p>
+
+    </div>
+  );
+}
+
+
+export default Settings;
